8604a2ef4d79a04c8e67e1bddfb0579ee1b8359f
ed16 <e.zaynullin@gmail.com>
2023-10-13 22:24:07 +0200

    test push

--- cmd/main.go
+++ cmd/main.go
@@ -31,7 +31,7 @@ var (
 
 var client *mongo.Client
 
-func main() {
+func main() { //test push
 	var err error
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
